06cc020731730690d34cab9771e6ac4cebb8f941
Ashish19025 <Ashishkumar19025@gmail.com>
2026-06-05 19:53:40 +0530

    feat:-labeling some functions and installation of expo-secure-store

--- src/context/TabBarContext.tsx
+++ src/context/TabBarContext.tsx
@@ -1,7 +1,11 @@
 import { createContext } from "react";
 
+// TabBarContext is a context object that provides a function to hide or show the tab bar.
 export const TabBarContext = createContext<{
+    // istabbarvisible is a function that takes a boolean value (hidden) and returns void.
+    // This function can be used to hide or show the tab bar based on the value of hidden.
     istabbarvisible: (hidden:boolean) => void;
 }>({
+    // The default value for istabbarvisible is an empty function that does nothing.
     istabbarvisible: () => {},
 });
